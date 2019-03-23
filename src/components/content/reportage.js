a297565a131ec94cb205fc8d94e56f7904698050
Kitty Jacobs <kitty.jacobs@immalle.eu>
2019-03-23 13:27:10 +0100

    Change class to className

--- src/components/content/reportage.js
+++ src/components/content/reportage.js
@@ -31,7 +31,7 @@ export default class Reportage extends React.Component{
                             <Header as="h2"color="grey">Stageverslagen </Header>
                             <Divider />
 
-                            <div class="text-20">
+                            <div className="text-20">
                                 Hier vindt u mijn dagverslagen. Het is een overzicht over wat ik elke dag gedaan heb op mijn stage.
                             </div>
                             <br></br>
@@ -40,15 +40,15 @@ export default class Reportage extends React.Component{
                             <Grid columns={2} divided>
                                 <Grid.Row>
                                     <Grid.Column>
-                                        <img class="ui centered large image rounded" src='img/gebouw.jpg' alt="a"/>
-                                        <div class="text-20">
+                                        <img className="ui centered large image rounded" src='img/gebouw.jpg' alt="a"/>
+                                        <div className="text-20">
                                             <br></br>
                                             Voici mon stage à Beerse! Je sais à quoi vous pensez. Ce n’est pas un bâtiment très grand et oui, c’est vrai. C’est la maison de l’entrepreneuse de LBit à Beerse.
                                         </div>
                                     </Grid.Column>
                                     <Grid.Column>
-                                        <img class="ui centered large image rounded" src='img/laptop.jpg' alt="a"/>
-                                        <div class="text-20">
+                                        <img className="ui centered large image rounded" src='img/laptop.jpg' alt="a"/>
+                                        <div className="text-20">
                                             <br></br>
                                             La plupart du temps je travaille ici sur mon ordinateur portable. Leen, l’entrepreneuse, m’a donné cet ordinateur pour travailler. Elle est trop gentille!
                                         </div>
@@ -56,15 +56,15 @@ export default class Reportage extends React.Component{
                                 </Grid.Row>
                                 <Grid.Row>  
                                         <Grid.Column>
-                                            <img class="ui centered large image rounded" src='img/leen.jpg' alt="a"/>
-                                            <div class="text-20">
+                                            <img className="ui centered large image rounded" src='img/leen.jpg' alt="a"/>
+                                            <div className="text-20">
                                                 <br></br>
                                                 Normalement, Leen travaille toujours seule mais pour cette semaine je l’accompagne pendant les heures de travail. Sans moi, elle n’a pas d’autres personnes dans la maison.
                                             </div>
                                         </Grid.Column>
                                         <Grid.Column>
-                                            <img class="ui centered large image rounded" src='img/printer.jpg' alt="a"/>
-                                            <div class="text-20">
+                                            <img className="ui centered large image rounded" src='img/printer.jpg' alt="a"/>
+                                            <div className="text-20">
                                                 <br></br>
                                                 Notre imprimante! Ah oui, tu restes toujours dans ma mémoire! Je me rappelle les moments quand tu as fait les sons: “beep… beep… beep....”. Très magnifique!
                                             </div>
@@ -72,15 +72,15 @@ export default class Reportage extends React.Component{
                                 </Grid.Row>
                                 <Grid.Row>  
                                         <Grid.Column>
-                                            <img class="ui centered large image rounded" src='img/lunch.jpg' alt="a"/>
-                                            <div class="text-20">
+                                            <img className="ui centered large image rounded" src='img/lunch.jpg' alt="a"/>
+                                            <div className="text-20">
                                                 <br></br>
                                                 Notre imprimante! Ah oui, tu restes toujours dans ma mémoire! Je me rappelle les moments quand tu as fait les sons: “beep… beep… beep....”. Très magnifique!
                                             </div>
                                         </Grid.Column>
                                         <Grid.Column>
-                                            <img class="ui centered large image rounded" src='img/kitty.jpg' alt="a"/>
-                                            <div class="text-20">
+                                            <img className="ui centered large image rounded" src='img/kitty.jpg' alt="a"/>
+                                            <div className="text-20">
                                                 <br></br>
                                                 Vous pouvez voir que je suis très concentrée. C’est entre moi et mes tâches de l’école et je gagnerai! Rien ne peut m’arrêter!
                                             </div>
