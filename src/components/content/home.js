e8df14c255150a13735ddeb5f60d897af9ee71cb
Kitty Jacobs <kitty.jacobs@immalle.eu>
2019-05-11 18:57:07 +0200

    Remove unnecessary item

--- src/components/content/home.js
+++ src/components/content/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Container, Header, Segment, Grid, Divider, Label, Image} from "semantic-ui-react";
+import {Container, Header, Segment, Grid, Divider, Label} from "semantic-ui-react";
 
 export default class Home extends React.Component{
     constructor(props) {
